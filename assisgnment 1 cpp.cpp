b1c328009474f05fb22b45c0a37ad7a1fe9c8ffd
monikadhingra011 <123304280+monikadhingra011@users.noreply.github.com>
2023-03-18 11:50:14 +0530

    Add files via upload

---
+++ assisgnment 1 cpp.cpp
@@ -0,0 +1,56 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 
+//   cout<<"Physiccs";
+//   cout<<"Wallah";
+    // 
+//   return 0;
+//   }
+   
+
+   // question 2nd 
+
+
+   // #include<iostream>
+   // using namespace std;
+   // int main(){
+//
+   //     cout<<5+5;
+//
+   //     return 0;
+//
+   // }
+
+
+
+
+   // question  no 3rd 
+
+   //int   4bytes 
+   //bool  1 bytes 
+   //float  4 bytes 
+
+   // 4th ans 64
+
+
+
+   // 5th question 
+
+   #include<iostream>
+   using namespace std;
+
+   int main(){
+   int r=10;
+    int pi= 3.14;
+
+    int cir = 2*pi*r ;
+    cout<<cir;
+    return 0;
+    }
+
+
+
+// 6th 
+
+// 2nd and 4th are correct 
